06c7b0465f4a34b5a2252d15e2ebeea35a656338
rajuazure123 <rajuazure123@gmail.com>
2026-05-16 22:51:22 +0530

    Fixed QA bugs

--- app/login.js
+++ app/login.js
@@ -1,3 +1,11 @@
-const login = "Login v1";
+const login = "Login v2 with API - Bug Fixed";
 
 console.log(login);
+
+const api = "Login API Added";
+
+console.log(api);
+
+const status = "QA Passed";
+
+console.log(status);
